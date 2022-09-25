6b961ced0e5228953407d46a0dfe51f98817c082
Ravi Panchal <80586465+panchalravi004@users.noreply.github.com>
2022-09-25 17:59:39 +0530

    Create a splashscreen UI

---
+++ SPM/app/src/main/java/com/govt/spm/SplashScreen.java
@@ -0,0 +1,20 @@
+package com.govt.spm;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SplashScreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        startActivity(new Intent(this,MainActivity.class));
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        startActivity(new Intent(this,MainActivity.class));
+    }
+}
