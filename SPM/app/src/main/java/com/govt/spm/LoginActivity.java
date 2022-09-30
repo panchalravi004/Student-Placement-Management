77a2c7ef38f12c378f775e877c18fd604a99f236
Ravi Panchal <80586465+panchalravi004@users.noreply.github.com>
2022-09-30 15:43:42 +0530

    Update Login UI and setup java file for Login

--- SPM/app/src/main/java/com/govt/spm/LoginActivity.java
+++ SPM/app/src/main/java/com/govt/spm/LoginActivity.java
@@ -2,19 +2,77 @@ package com.govt.spm;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class LoginActivity extends AppCompatActivity {
+
+    private EditText etUserId,etPassword;
+    private Button btnLogin;
+    private SharedPreferences userPref;
+    private SharedPreferences.Editor editor;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        etUserId = (EditText) findViewById(R.id.etUserIdLogin);
+        etPassword = (EditText) findViewById(R.id.etPasswordLogin);
+        btnLogin = (Button) findViewById(R.id.btnLogin);
+        userPref = getSharedPreferences("user",MODE_PRIVATE);
+        editor = userPref.edit();
+
+    }
+    //Do login when click
+    public void doLogin(View view) {
+        String userid = etUserId.getText().toString();
+        String password = etPassword.getText().toString();
+
+        //start loading
+        ProgressDialog pd = new ProgressDialog(LoginActivity.this);
+        pd.setMessage("Authenticating...");
+        pd.show();
+
+        if(validate()){
+            //Here actual login auth - and fetch user role
+            //According user role - use intent to move to next activity
+            editor.putString("userId",userid);
+            editor.putString("role","TPO");
+            editor.apply();
+
+            startActivity(new Intent(getBaseContext(),OfficerDashboardActivity.class));
+            finish();
+        }else{
+            pd.dismiss();
+        }
     }
 
-    public void goToDashboard(View view) {
-        startActivity(new Intent(this,OfficerDashboardActivity.class));
+    //validating fields
+    private boolean validate(){
+        String userid = etUserId.getText().toString();
+        String password = etPassword.getText().toString();
+
+        if (userid.equals("")) {
+            Toast.makeText(this, "Please Enter User Id", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+        if (password.equals("")) {
+            Toast.makeText(this, "Please Enter Password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
     }
+
+    //For Authentication and fetch user role : ex : student , TPO
+    private void Authentication(){
+
+    }
+
 }
--- SPM/app/src/main/java/com/govt/spm/SplashScreen.java
+++ SPM/app/src/main/java/com/govt/spm/SplashScreen.java
@@ -1,20 +1,34 @@
 package com.govt.spm;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashScreen extends AppCompatActivity {
+
+    SharedPreferences userPref;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 //        startActivity(new Intent(this,MainActivity.class));
+        userPref = getSharedPreferences("user",MODE_PRIVATE);
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        startActivity(new Intent(this,LoginActivity.class));
+        if(userPref.contains("userId")){
+            if(userPref.getString("role","role").equals("TPO")){
+                startActivity(new Intent(this,OfficerDashboardActivity.class));
+            }
+            else if(userPref.getString("role","role").equals("STUDENT")){
+                startActivity(new Intent(this,StudentDashboardActivity.class));
+            }
+
+        }else{
+            startActivity(new Intent(this,LoginActivity.class));
+        }
     }
 }
