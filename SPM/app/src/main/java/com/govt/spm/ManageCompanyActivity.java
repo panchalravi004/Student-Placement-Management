0e6b1c9efba2eb0e8a878d9638bf9ba93108b416
Ravi Panchal <80586465+panchalravi004@users.noreply.github.com>
2022-09-30 17:29:17 +0530

    Update TPO Manage Company UI & Java

--- SPM/app/src/main/java/com/govt/spm/ManageCompanyActivity.java
+++ SPM/app/src/main/java/com/govt/spm/ManageCompanyActivity.java
@@ -5,18 +5,31 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.ListView;
+import android.widget.Spinner;
 
 import java.util.ArrayList;
 
 public class ManageCompanyActivity extends AppCompatActivity {
+    EditText etSearch;
+    ImageButton btnSearch;
+    Spinner spFilterOne,spFilterTwo;
+
     ListView company_list;
     CompanyAdapter ca;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_manage_company);
-        company_list = (ListView) findViewById(R.id.company_list);
+
+        btnSearch = (ImageButton) findViewById(R.id.btnManageCompanySearch);
+        etSearch = (EditText) findViewById(R.id.etManageCompanySearch);
+        spFilterOne = (Spinner) findViewById(R.id.spManageCompanyFilterOne);
+        spFilterTwo = (Spinner) findViewById(R.id.spManageCompanyFilterTwo);
+
+        company_list = (ListView) findViewById(R.id.list_manage_company_company);
 
         ArrayList<String> cname = new ArrayList<String>();
         cname.add("Info it Solution One");
@@ -31,7 +44,7 @@ public class ManageCompanyActivity extends AppCompatActivity {
         finish();
     }
 
-    public void goToAddJobs(View view) {
+    public void goToAddCompany(View view) {
         startActivity(new Intent(ManageCompanyActivity.this,AddCompanyActivity.class));
     }
 }
